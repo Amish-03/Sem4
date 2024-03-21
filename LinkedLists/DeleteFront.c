88ec3137f5b968d04845c8b052f7e1a35fb6d1a9
Amish-03 <163165606+Amish-03@users.noreply.github.com>
2024-03-21 07:44:22 +0530

    Add files via upload

---
+++ LinkedLists/DeleteFront.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}*NODE;
+
+NODE getnode()
+{
+    NODE newn=(NODE)malloc(sizeof(struct Node));
+    if(newn==NULL)
+        printf("Memory not allocated\n");
+    newn->next=NULL;
+    return newn;
+}
+
+NODE insert_end(NODE head)
+{
+   NODE end;
+   end=getnode();
+   NODE cur=head;
+   printf("Enter data for new node\n");
+   scanf("%d",&end->data);
+   end->next=NULL;
+   if(head==NULL)
+    return end;
+    else{
+   while(cur->next!=NULL)
+   {
+       cur=cur->next;
+   }
+   cur->next=end;
+   end->next=NULL;
+   return head;
+    }
+}
+
+NODE deleteFront(NODE head)
+{
+   NODE cur=head;
+    if(head==NULL)
+    {
+        printf("List empty\n");
+        return NULL;
+    }
+    else if(head->next==NULL)
+    {
+        printf("%d is deleted\n",head->data);
+        return NULL;
+    }
+    else
+    {
+        head=head->next;
+        printf("%d is deleted\n",cur->data);
+
+        free(cur);
+        return head;
+    }
+
+}
+
+int main()
+{
+    NODE head=NULL;
+    NODE cur=head;
+    int choice;
+
+     while(1)
+    {
+        cur=head;
+        printf("1:Insert End\n2:print\n3:Delete front\n");
+        scanf("%d",&choice);
+        if(choice==1)
+        {
+            head=insert_end(head);
+        }
+        else if(choice==2)
+        {
+            if(head==NULL)
+                printf("List empty\n");
+            else
+            {
+
+            while(cur!=NULL)
+            {
+                printf("%d -> ",cur->data);
+                cur=cur->next;
+            }
+            printf("NULL\n");
+        }
+        }
+        else if(choice==3)
+        {
+            head=deleteFront(head);
+        }
+        else
+            printf("Invalid!! Try again\n");
+    }
+}
